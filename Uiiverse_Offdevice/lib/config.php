f1a02a65e108d54d89659e6c9344606d5d4b90a2
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2019-12-29 20:54:18 -0500

    Add Maintenance Mode

--- Uiiverse_Offdevice/lib/config.php
+++ Uiiverse_Offdevice/lib/config.php
@@ -8,6 +8,7 @@ const UII_MYSQL_PASS = "";
 const UII_MYSQL_DB = "uiiverse_db";
 
 // Discovery Server API
+const UII_DISCOVERY_MAINTENANCE_MODE = false;
 const UII_DISCOVERY_API_HOST = "";
 const UII_DISCOVERY_PORTAL_HOST = "";
 const UII_DISCOVERY_N3DS_HOST = "";
