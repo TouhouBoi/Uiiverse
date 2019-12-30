b0b62049c27c2cc0afc15592714fd82f222ee395
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2019-12-29 20:45:07 -0500

    Add Discovery Server Config Stuff

--- Uiiverse_Offdevice/lib/config.php
+++ Uiiverse_Offdevice/lib/config.php
@@ -1,10 +1,13 @@
 <?php
 // Uiiverse Config
 
+// MySQL Database
 const UII_MYSQL_HOST = "localhost";
-
 const UII_MYSQL_USER = "root";
-
 const UII_MYSQL_PASS = "";
-
 const UII_MYSQL_DB = "uiiverse_db";
+
+// Discovery Server API
+const UII_DISCOVERY_API_HOST = "";
+const UII_DISCOVERY_PORTAL_HOST = "";
+const UII_DISCOVERY_N3DS_HOST = "";
