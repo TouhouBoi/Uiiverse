79010010dc45bda8ba57bd2e565b47b4ad1edf40
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2019-12-30 22:53:06 -0500

    Update config.php

--- Uiiverse_Offdevice/lib/config.php
+++ Uiiverse_Offdevice/lib/config.php
@@ -1,15 +1,8 @@
 <?php
-// Uiiverse Config
+// Uiiverse Offdevice Config
 
 // MySQL Database
 const UII_MYSQL_HOST = "localhost";
 const UII_MYSQL_USER = "root";
 const UII_MYSQL_PASS = "";
 const UII_MYSQL_DB = "uiiverse_db";
-
-// Discovery Server API
-const UII_DISCOVERY_SERVICE_CLOSED = false;
-const UII_DISCOVERY_MAINTENANCE_MODE = false;
-const UII_DISCOVERY_API_HOST = "";
-const UII_DISCOVERY_PORTAL_HOST = "";
-const UII_DISCOVERY_N3DS_HOST = "";
