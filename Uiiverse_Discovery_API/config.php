c7e2a45bcd1a1b8b2cfc1c83270b65da4e334fb1
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2019-12-30 22:52:42 -0500

    Update config.php

---
+++ Uiiverse_Discovery_API/config.php
@@ -0,0 +1,15 @@
+<?php
+// Uiiverse Discovery Server Config
+
+// Uiiverse MySQL Database
+const UII_MYSQL_HOST = "localhost";
+const UII_MYSQL_USER = "root";
+const UII_MYSQL_PASS = "";
+const UII_MYSQL_DB = "uiiverse_db";
+
+// Discovery Server API
+const UII_DISCOVERY_SERVICE_CLOSED = false;
+const UII_DISCOVERY_MAINTENANCE_MODE = false;
+const UII_DISCOVERY_API_HOST = "";
+const UII_DISCOVERY_PORTAL_HOST = "";
+const UII_DISCOVERY_N3DS_HOST = "";
