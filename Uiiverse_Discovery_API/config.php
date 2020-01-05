168626780201c773bd9b1a70e282d4998c9318e7
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2020-01-05 18:13:18 -0500

    Add Example Urls

--- Uiiverse_Discovery_API/config.php
+++ Uiiverse_Discovery_API/config.php
@@ -6,6 +6,6 @@ const UII_DISCOVERY_SERVICE_CLOSED = false;
 const UII_DISCOVERY_MAINTENANCE_MODE = false;
 
 // Discovery Server API Urls
-const UII_DISCOVERY_API_HOST = "";
-const UII_DISCOVERY_PORTAL_HOST = "";
-const UII_DISCOVERY_N3DS_HOST = "";
+const UII_DISCOVERY_API_HOST = "api.olv.example.com";
+const UII_DISCOVERY_PORTAL_HOST = "portal.olv.example.com";
+const UII_DISCOVERY_N3DS_HOST = "n3ds.olv.example.com";
